7fae338052da8f25e1e651895bc3aa35756f8bc6
Tiana Todos <tiana.todos@gmail.com>
2016-10-25 22:16:47 -0700

    starting point

---
+++ rafRunner.py
@@ -0,0 +1,10 @@
+reated on Oct 25, 2016
+@author: tiana
+'''
+
+from rafUtils import getLog,qaprint
+
+log = getLog()
+
+message = "It is working, right?"
+qaprint(log,message)
---
+++ rafUtils.py
@@ -0,0 +1,9 @@
+created on Oct 25, 2016
+
+@author: tiana
+'''
+def getLog():
+    
+   return 1
+  
+def qaprint(): 
