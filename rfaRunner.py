0dc9e9ec717ffadb1c811c819e8dd1362754494c
Tanya Cora <tanya.projects@gmail.com>
2016-11-02 00:45:55 -0700

    task2.v2 - tasks getLocalEnv() and parsing args are implemented

--- rfaRunner.py
+++ rfaRunner.py
@@ -11,9 +11,10 @@ import sys
 # parsing arguments
 if len(sys.argv) != 2:
     sys.exit("Invalid number of arguments. rfaRunner accepts one argument")
+tr_name = sys.argv[0]
 arg = sys.argv[1].split("=")
 if arg[0].lower() == "--testrun":
-    if 0 <= int(arg[1]) <= 10000:
+    if int(arg[1]) in range(0,10001):
         trid = arg[1]
     else:
         sys.exit("Invalid test run number. Valid is in [0-10000]") 
@@ -22,11 +23,16 @@ else:
 
 test_cases = getTestCases(trid)
 if test_cases == -1:
-    sys.exit("Unable to read test cases from testrun file")
+    sys.exit("Unable to read test cases from %s file" % trid)
+print test_cases
+env = getLocalEnv("local.properties")
+if env == -1:
+    sys.exit("Unable to read the %s file" % "local.properties")
 
+log_location = env['log_dir']
 
 # get the log file handle
-log = getLog(trid)
+log = getLog(log_location,tr_name)
 
 # exit if log creation failed
 if log == -1:
--- rfaUtils.py
+++ rfaUtils.py
@@ -10,24 +10,21 @@ import sys
 import traceback
 
 
-def getLog(filename):
+def getLog(location,filename):
     """
     Creates 'logs' directory, if it doesn't exist,
     creates or opens a log file in 'logs' directory.
     """
     # assign a current working dir + '/logs' to log_dir var (OS independent)
-    log_dir = os.path.join(os.getcwd(), "logs")
-    # or --> script directory: 
-    #log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
-    # or --> user directory: 
-    #log_dir = os.path.join(os.path.expanduser("~"), "logs")
-
+    log_dir = os.path.join(os.getcwd(), location)
+    name = filename.split(".")
+    tr_name = name[0]
     try:
         # if logs directory(!) doesn't exist, create it
         if not os.path.isdir(log_dir):
             os.makedirs(log_dir)
         # open log file with prefix and timestamp (OS independ.) in Append mode
-        log = open(os.path.join(log_dir, "rfaRunner_" \
+        log = open(os.path.join(log_dir, tr_name + "_" \
                                 + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
         return log
     except (OSError, IOError):
@@ -55,23 +52,47 @@ def getCurTime(date_time_format):
     date_time = datetime.now().strftime(date_time_format)
     return date_time
 
+
+def getLocalEnv(env_file):
+    #check if file exists
+    envfile_dir = os.path.join(os.getcwd())
+    envfile_name = os.path.join(envfile_dir, env_file)
+    properties = dict()
+    if not os.path.isfile(envfile_name):
+        sys.exit("%s file doesn't exist" % envfile_name)
+    else:
+        with open(envfile_name, "r") as env_file:
+            for line in env_file:
+                try:
+                    line = line.split("=")
+                    properties[line[0].strip()] = line[1].strip()
+                except (OSError, IOError):
+                    return -1
+    
+    return properties            
+                
+
+
 def getTestCases(trid):
     keys = ["tcid", "rest_URL", "HTTP_method", "HTTP_RC_desired", "param_list"]
-    try:
-        # if file doesn't exist, exit
-        testrun_dir = os.path.join(os.getcwd())
-        testrun_name = os.path.join(testrun_dir, trid) + ".txt"
-        testrun_handle = open(testrun_name, "r")
-        if not os.path.isfile(testrun_name):
-            sys.exit("Testrun file doesn't exist")
-        else:
-            for line in testrun_handle:
-                elements = line.split("|")
-                testrun_properties = dict(zip(keys, elements))
-            testrun_handle.close()
+    testrun_properties = dict()
+    # if file doesn't exist, exit
+    testrun_dir = os.path.join(os.getcwd())
+    testrun_name = os.path.join(testrun_dir, trid) + ".txt"
+    if not os.path.isfile(testrun_name):
+        sys.exit("%s file doesn't exist" % testrun_name)
+    else:
+        try:
+            with open(testrun_name, "r") as testrun_handle:
+                for line in testrun_handle:
+                    elements = line.split("|")
+                    print elements
+                    #testrun_properties = dict(zip(keys, elements))
             return testrun_properties
+        except (OSError, IOError):
+            return -1 
+
         
-    except (OSError, IOError):
-        return -1 
+
 
     
