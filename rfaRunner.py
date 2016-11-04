be3a5856240afb7dc6dfe298edbd8934b8146bd7
natasha.sm <natasha.sm@gmail.com>
2016-11-03 19:31:59 -0700

    “hw2”

--- rfaRunner.py
+++ rfaRunner.py
@@ -1,8 +1,52 @@
-from rfaUtils import getLog,qaprint
+'''
+Created on Oct 19, 2016
 
-log = getLog()
-if log == -1:
-    print "Sorry, there is a problem with a log file"
+
+@author: sashaalexander
+@author: team X
+'''
+from rfaUtils import getLog, qaPrint, getLocalEnv, getTestCases
+
+import sys
+
+
+# ---------------------------------
+filename =  sys.argv[0]
+if len(sys.argv) < 2:
+    sys.exit("Incorrect request was entered, example of correct one: rfaRunner.py --testrun=42")
 else:
-    message = "It is working, right?"
-    qaprint(log, message)
+    testId = sys.argv[1].split("=")
+    trid =  int(testId[1])
+
+
+fileToDict = getLocalEnv('local.properties')
+if fileToDict  == -1:
+    sys.exit("Can't create a dictionary: the file local.properties is empty or doesn't exist")
+else:
+    # get the log file handle
+    log = getLog(fileToDict['log_dir'], filename)
+
+#===============================
+# exit if log creation failed
+if log == -1:
+    sys.exit("Unable to create log file")
+
+message = "It is working, right?"
+
+# call qaPrint to print a message with timestamp and write it to the log file
+qaPrint(log, message)
+#qaPrint(log, "Me like what me see")
+
+
+    
+myDict = getTestCases(trid)
+if myDict  == -1:
+    qaPrint(log, "Can't create a dictionary: the file is empty or doesn't exist")
+
+    
+# close the log file if it open
+if not log.closed:
+    log.close()
+
+
+    
